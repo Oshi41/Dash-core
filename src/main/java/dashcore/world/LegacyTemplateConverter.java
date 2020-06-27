994a1f13191f82834daecac4d713ef6ebe7ef313
oshi4 <sheglovaoe@gmail.com>
2020-06-27 04:00:16 +0300

    Legacy covnerter

---
+++ src/main/java/dashcore/world/LegacyTemplateConverter.java
@@ -0,0 +1,29 @@
+package dashcore.world;
+
+import dashcore.world.interfaces.IChunkStorage;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.gen.feature.WorldGenerator;
+
+import java.io.File;
+import java.util.Map;
+
+public class LegacyTemplateConverter {
+
+    public static void convert(WorldGenerator generator, File folder) {
+        MockWorld world = new MockWorld();
+        generator.generate(world, world.rand, BlockPos.ORIGIN);
+        Map<ChunkPos, IChunkStorage> chunks = world.chunks;
+
+        if (folder.mkdirs()) {
+            if (folder.mkdir()) {
+                for (IChunkStorage storage : chunks.values()) {
+                    Template template = new Template(storage);
+                    ChunkPos pos = storage.getPos();
+                    File file = new File(folder, String.format("[%s,%s].nbt", pos.x, pos.z));
+                    template.writeToFile(file);
+                }
+            }
+        }
+    }
+}
